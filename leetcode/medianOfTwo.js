8e2d6868f1f7ee9b5f202727373c863a738773db
HalilCan <halilcanmemoglu@gmail.com>
2020-01-13 23:03:00 +0300

    first solution

--- leetcode/medianOfTwo.js
+++ leetcode/medianOfTwo.js
@@ -9,27 +9,56 @@ medianOfTwo(array1, array2) {
         return medianSingleton(array2[0], array1);
     }
 
-
+    let mid1 = Math.floor(len1/2);
+    let mid2 = Math.floor(len2/2);
+    if (array1[mid1] <= array2[mid2]){
+        return medianOfTwo(array1.slice(mid1, len1), array2.slice(mid2));
+    } else {
+        return medianOfTwo(array1.slice(mid1), array2.slice(mid2, len2));
+    }
 }
-
-[1,5]
-[3]
 
 medianSingleton(num, array) {
     let len = array.length;
+    let mid = Math.floor(len/2);
     if (len == 1) {
         return ((num + array[0]) / 2);
     }
 
-    if (array[Math.floor(len/2)] > num) {
-        return adjustedMedian(1, array);
-    } else if (array[Math.floor(len/2) + 1] < num) {
-        return adjustedMedian(-1, array);
+    if (len % 2 == 0) {
+        if (num < array[mid]) {
+            if (num > array[mid - 1]) {
+                return num;
+            } else {
+                return array[mid - 1];
+            }
+        } else {
+            if (len > 2) {
+                if (num < array[mid + 1]) {
+                    return num;
+                } else {
+                    return array[mid + 1];
+                }
+            } else {
+                return array[mid]
+            }
+        }
     } else {
-        
+        if (num < array[mid]) {
+            if (num > array[mid - 1]) {
+                return (num + array[mid]) / 2;
+            } else {
+                return (array[mid-1] + array[mid]) / 2;
+            }
+        } else {
+            if (num < array[mid + 1]) {
+                return (num + array[mid]) / 2;
+            } else {
+                return (array[mid + 1] + array[mid]) / 2;
+            }
+        }
     }
+
 }
 
-adjustedMedian(adjustment, array) {
-    return array[Math.floor((array.length + adjustment) / 2)];   
-}
+
