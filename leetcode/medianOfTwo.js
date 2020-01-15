e75351511da4760da652ee80162e4231085a3301
HalilCan <halilcanmemoglu@gmail.com>
2020-01-15 08:20:52 -0500

    medianOfTwo done

--- leetcode/medianOfTwo.js
+++ leetcode/medianOfTwo.js
@@ -8,13 +8,23 @@ function medianOfTwo(array1, array2) {
     if (len2 == 1) {
         return medianSingleton(array2[0], array1);
     }
+    if (len1 == 2 && len2 == 2) {
+        return (Math.max(array1[0], array2[0]) + Math.min(array1[1], array2[1])) / 2
+    }
 
     let mid1 = Math.floor(len1/2);
     let mid2 = Math.floor(len2/2);
-    if (array1[mid1] <= array2[mid2]){
-        return medianOfTwo(array1.slice(mid1, len1), array2.slice(mid2));
+    if (medianOfOne(array1) < medianOfOne(array2)){//array1[mid1] < array2[mid2]){
+        console.log(`<`);
+        console.log(array1.slice(mid1, len1), array2.slice(0, mid2+1));
+        return medianOfTwo(array1.slice(mid1, len1), array2.slice(0, mid2+1));
+    } else if (medianOfOne(array1) > medianOfOne(array2)){//array1[mid1] > array2[mid2]){
+        console.log(`>`);
+        console.log(array1.slice(0, mid1+1), array2.slice(mid2, len2));
+        return medianOfTwo(array1.slice(0, mid1+1), array2.slice(mid2, len2));    
+        
     } else {
-        return medianOfTwo(array1.slice(mid1), array2.slice(mid2, len2));
+        return array1[mid1];
     }
 }
 
@@ -26,7 +36,7 @@ function medianSingleton(num, array) {
     }
 
     if (len % 2 == 0) {
-        if (num < array[mid]) {
+        if (num < medianOfOne(array)) {
             if (num > array[mid - 1]) {
                 return num;
             } else {
@@ -34,13 +44,13 @@ function medianSingleton(num, array) {
             }
         } else {
             if (len > 2) {
-                if (num < array[mid + 1]) {
+                if (num < array[mid]) {
                     return num;
                 } else {
-                    return array[mid + 1];
+                    return array[mid];
                 }
             } else {
-                return array[mid]
+                return array[mid - 1]
             }
         }
     } else {
@@ -58,10 +68,21 @@ function medianSingleton(num, array) {
             }
         }
     }
-
 }
 
-let TEST_ARRS = [[1,2,3], [-3,-4,-5], [-100,101,102], [-1,22], [-5,6,12,17], [0, 10000000, 20000000], [100000], [1]];
+function medianOfOne(array) {
+    let len = array.length;
+    let mid = Math.floor(array.length / 2);
+    let ret;
+    if (len % 2 == 0) {
+        ret = (array[mid] + array[mid - 1]) / 2;
+    } else {
+        ret =  (array[mid]);
+    }
+    return ret;
+}
+
+let TEST_ARRS = [[1,2,3], [-5,-4,-3], [-100,101,102], [-1,22], [-5,6,12,17], [0, 10000000, 20000000], [-100000, 100000], [1]];
 let TEST_COUNT = 10;
 
 function test() {
@@ -73,4 +94,5 @@ function test() {
     }
 }
 
+//console.log(TEST_ARRS[6], TEST_ARRS[5], medianOfTwo(TEST_ARRS[6], TEST_ARRS[5]));
 test();
